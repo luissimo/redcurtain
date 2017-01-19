ccb8bf9d822f2a494d6e35f74e02e3f78fefde50
luissimo <khijbar@gmail.com>
2017-01-19 15:41:11 +0100

    invoices index added

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170119133926) do
+
+  create_table "invoices", force: :cascade do |t|
+    t.string   "number"
+    t.string   "currency"
+    t.date     "date"
+    t.date     "duedate"
+    t.decimal  "btwtotal"
+    t.decimal  "subtotal"
+    t.decimal  "total"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
